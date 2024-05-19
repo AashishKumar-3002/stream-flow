cb78f63b463911ac779e934c7bc7973d174b0e3b
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-05-19 14:46:39 +0530

    final phase ready : main.py for cmd-line mode and app.py for api model

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import argparse
+from flask import Flask, request, jsonify
 import concurrent.futures
 from scripts.extract_videos import download_video_audio
 from scripts.transcribe_videos import video_transcribe
 from scripts.store_transcriptions import update_transcription
 from scripts.upsert_transcription import upsert
 from scripts.final_processing import fromat_transcription
+
+app = Flask(__name__)
 
 def process_url(url):
     # Download the video and audio
@@ -20,24 +22,22 @@ def process_url(url):
     # Upsert the transcription to Google Sheets
     upsert(video_title)
 
-def main(urls):
+@app.route('/process', methods=['POST'])
+def process_videos():
+    urls = request.json.get('urls', [])
+    if not urls:
+        return jsonify({"error": "No URLs provided"}), 400
+    
     # Use a ThreadPoolExecutor to download, transcribe and rename the videos in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(process_url, urls)
-
+    
     # Use the final_processing.py script
     processed_files = fromat_transcription(urls)
     if len(processed_files) == len(urls):
-        print("All files processed successfully")
+        return jsonify({"message": "All files processed successfully", "processed_files": processed_files}), 200
     else:
-        print("Some files failed to process. You can try them later")
-        print("Processed files:", processed_files)
+        return jsonify({"message": "Some files failed to process. You can try them later", "processed_files": processed_files}), 206
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Process video URLs.")
-    parser.add_argument('urls', nargs='+', help='List of video URLs to process')
-    args = parser.parse_args()
-    main(args.urls)
-
-    # Example usage:
-    # python main.py https://www.instagram.com/reel/C0eV7_fCYKs/?igsh=MXRsZGszODRobnJ1MA== https://www.instagram.com/reel/C6wcq9xCAaX/
+    app.run(debug=True)
